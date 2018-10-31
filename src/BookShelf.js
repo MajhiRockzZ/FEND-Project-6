355e9f3c2eabe198c0f3373c63f54566229d85ac
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-10-31 14:26:35 +0530

    feat: Add books to display
    Setting up bookToDisplay inside BookShelf.

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import Book from './Book';
+import PropTypes from 'prop-types';
+
+const BookShelf = (props) => {
+    const booksToDisplay = props.book.map((book) => (
+        <li key={book.id}>
+            <Book
+
+            />
+        </li>
+    ))
+}
