29ed38e225be87003f26b77a2e9b6e43afcae94e
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-10-31 17:41:33 +0530

    feat: Add ListBooks component
    Adding ListBooks component with propTypes.

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -3,3 +3,17 @@ import {Link} from 'react-router-dom';
 import BookShelf from './BookShelf';
 import ShelfSelect from '/ShelfSelect';
 import PropTypes from 'prop-types';
+
+class ListBooks extends Component {
+    bulkShelfChange = (event) => {
+        const targetShelf = event.target.value;
+        this.props.onBulkShelfChange(targetShelf);
+    }
+}
+
+ListBooks.propTypes = {
+    books: PropTypes.array.isRequired,
+    onShelfChange: PropTypes.func.isRequired,
+};
+
+export default ListBooks;
