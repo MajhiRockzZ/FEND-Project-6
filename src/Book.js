78649cdf6cd7855014eb9658997784e801354d59
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-10-31 15:19:18 +0530

    feat: Export book components
    Exporting Book components for bookshelf.

--- src/Book.js
+++ src/Book.js
@@ -1,20 +1,29 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
-render() {
-    const {id, title, thumbnail, shelf, authors, rating} = this.props;
+class Book extends Component {
+    render() {
+        const {id, title, thumbnail, shelf, authors, rating} = this.props;
 
-    return (
-        <div className='book'>
-            <div className='book-top'>
-                <div className='book-cover' style={{
-                    width: 128,
-                    height: 193,
-                    backgroundImage: `url(${thumbnail})`
-                }}>
-
+        return (
+            <div className='book'>
+                <div className='book-top'>
+                    <div className='book-cover' style={{
+                        width: 128,
+                        height: 193,
+                        backgroundImage: `url(${thumbnail})`
+                    }}>
+                    </div>
+                    <ShelfSelect onChange={this.changeShelf} value={shelf}/>
                 </div>
+                <div className='book-title'>{title}</div>
+                {authors && authors.map(author => (
+                    <div key={author} className='book-authors'>{author}</div>
+                ))}
             </div>
-        </div>
-    )
+        )
+    }
 }
+
+
+export default Book;
