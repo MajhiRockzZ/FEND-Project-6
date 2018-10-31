6b9c4efa274fcc7c9f0e27cb3e76bd914bb5c703
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-10-31 16:00:39 +0530

    feat: Update shelf for book
    Updating the shelf for a book.

--- src/App.js
+++ src/App.js
@@ -7,11 +7,37 @@ class BooksApp extends React.Component {
     state = {
         books: [],
         checkedBooks: []
-    }
+    };
 
     componentDidMount() {
         BooksAPI.getAll().then(books => {
             this.setState({books})
+        })
+    }
+
+    /**
+     * Update shelf for a book
+     */
+    updateShelf = (updatedBook, shelf) => {
+        BooksApp.update(updatedBook, shelf).then(response => {
+            this.setState(state => {
+                let currentBooks = [];
+                const bookFoundInLibrary = state.books.find(book =>
+                    book.id === updatedBook.id);
+
+                if (bookFoundInLibrary) {
+                    currentBooks = state.books.map(book => {
+                        if (book.id === updatedBook.id) {
+                            book.shelf = shelf;
+                        }
+                        return book;
+                    })
+                } else {
+                    updatedBook.shelf = shelf;
+                    currentBooks = state.books.concat([updatedBook]);
+                }
+                return {book: currentBooks};
+            })
         })
     }
 }
