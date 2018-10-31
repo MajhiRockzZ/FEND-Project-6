eee29819f5442f042cb317175ee6510fcdcc7965
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-10-31 15:45:58 +0530

    feat: Fetch book API
    Fetching book API from BooksAPI.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
+import {Route} from 'react-router-dom'
 import './App.css'
 
 class BooksApp extends React.Component {
     state = {
         books: [],
         checkedBooks: []
+    }
+
+    componentDidMount() {
+        BooksAPI.getAll().then(books => {
+            this.setState({books})
+        })
     }
 }
 
