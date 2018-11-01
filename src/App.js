6a92e7f5b8a39391e8245953d1a3fd2b633de0a0
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-11-01 22:22:06 +0530

    style: Formatting JS files
    Changing the intend space to 2.

--- src/App.js
+++ src/App.js
@@ -6,129 +6,129 @@ import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
-    state = {
-        books: [],
-        checkedBooks: []
-    };
+  state = {
+    books: [],
+    checkedBooks: []
+  };
 
-    componentDidMount() {
-        BooksAPI.getAll().then(books => {
-            this.setState({books})
-        })
-    }
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      this.setState({books})
+    })
+  }
 
-    /**
-     * Update shelf for a book
-     */
-    updateShelf = (updatedBook, shelf) => {
-        BooksApp.update(updatedBook, shelf).then(response => {
-            this.setState(state => {
-                let currentBooks = [];
-                const bookFoundInLibrary = state.books.find(book =>
-                    book.id === updatedBook.id);
+  /**
+   * Update shelf for a book
+   */
+  updateShelf = (updatedBook, shelf) => {
+    BooksApp.update(updatedBook, shelf).then(response => {
+      this.setState(state => {
+        let currentBooks = [];
+        const bookFoundInLibrary = state.books.find(book =>
+          book.id === updatedBook.id);
 
-                if (bookFoundInLibrary) {
-                    currentBooks = state.books.map(book => {
-                        if (book.id === updatedBook.id) {
-                            book.shelf = shelf;
-                        }
-                        return book;
-                    })
-                } else {
-                    updatedBook.shelf = shelf;
-                    currentBooks = state.books.concat([updatedBook]);
-                }
-                return {book: currentBooks};
-            })
-        })
-    };
+        if (bookFoundInLibrary) {
+          currentBooks = state.books.map(book => {
+            if (book.id === updatedBook.id) {
+              book.shelf = shelf;
+            }
+            return book;
+          })
+        } else {
+          updatedBook.shelf = shelf;
+          currentBooks = state.books.concat([updatedBook]);
+        }
+        return {book: currentBooks};
+      })
+    })
+  };
 
-    /**
-     * Bulk update shelf
-     */
+  /**
+   * Bulk update shelf
+   */
 
-    bulkUpdateShelf = (shelf) => {
-        const updatePromises = this.state.checkedBooks.map(book => BooksAPI.update(book, shelf));
+  bulkUpdateShelf = (shelf) => {
+    const updatePromises = this.state.checkedBooks.map(book => BooksAPI.update(book, shelf));
 
-        return Promise
-            .all(updatePromises)
-            .then(response => {
-                this.setState(state => {
-                    // Handling new books from search result
-                    const newBooks = state.checkedBooks.filter(
-                        checkedBook => {
-                            const existInLibrary = state.books.find(book =>
-                                book.id === checkedBook.id);
-                            return !existInLibrary;
-                        });
-                    const newBooksWithUpdatedShelf = newBooks.map(
-                        newBooks => {
-                            newBooks.shelf = shelf;
-                            return newBooks;
-                        });
-                    const currentBookWithUpdatedShelf = state.books.map(book => {
-                        const bookExistInCheckedBooks = state.checkedBooks.find(
-                            checkedBook => checkedBook.id === book.id);
-
-                        if (bookExistInCheckedBooks) {
-                            book.shelf = shelf;
-                        }
-
-                        return book;
-                    });
-
-                    return {
-                        book: currentBookWithUpdatedShelf.concat(newBooksWithUpdatedShelf),
-                        checkedBooks: [],
-                    }
-                })
-            })
-    };
-
-    /**
-     * Check/tick the book
-     */
-    checkBook = (checkedBook, checkedStatus) => {
+    return Promise
+      .all(updatePromises)
+      .then(response => {
         this.setState(state => {
-            const existInCheckedBooks = state.checkedBooks.find(book => book.id === checkedBook.id);
+          // Handling new books from search result
+          const newBooks = state.checkedBooks.filter(
+            checkedBook => {
+              const existInLibrary = state.books.find(book =>
+                book.id === checkedBook.id);
+              return !existInLibrary;
+            });
+          const newBooksWithUpdatedShelf = newBooks.map(
+            newBooks => {
+              newBooks.shelf = shelf;
+              return newBooks;
+            });
+          const currentBookWithUpdatedShelf = state.books.map(book => {
+            const bookExistInCheckedBooks = state.checkedBooks.find(
+              checkedBook => checkedBook.id === book.id);
 
-            if (!existInCheckedBooks && checkedStatus === true) {
-                return {
-                    checkedBooks: state.checkedBooks.concat([checkedBook])
-                }
+            if (bookExistInCheckedBooks) {
+              book.shelf = shelf;
             }
 
-            if (checkedStatus === false) {
-                return {
-                    checkedBooks: state.checkedBooks.filter(book => book.id !== checkedBook.id)
-                }
-            }
+            return book;
+          });
+
+          return {
+            book: currentBookWithUpdatedShelf.concat(newBooksWithUpdatedShelf),
+            checkedBooks: [],
+          }
         })
-    };
+      })
+  };
+
+  /**
+   * Check/tick the book
+   */
+  checkBook = (checkedBook, checkedStatus) => {
+    this.setState(state => {
+      const existInCheckedBooks = state.checkedBooks.find(book => book.id === checkedBook.id);
+
+      if (!existInCheckedBooks && checkedStatus === true) {
+        return {
+          checkedBooks: state.checkedBooks.concat([checkedBook])
+        }
+      }
+
+      if (checkedStatus === false) {
+        return {
+          checkedBooks: state.checkedBooks.filter(book => book.id !== checkedBook.id)
+        }
+      }
+    })
+  };
 
 
-    render() {
-        return (
-            <div className='app'>
-                <Route exact path='/' render={() =>
-                    <ListBooks
-                        books={this.state.books}
-                        onShelfChange={this.updateShelf}
-                        onBulkShelfChange={this.bulkUpdateShelf}
-                        onBookChecked={this.checkBook}
-                    />
-                }/>
-                <Route path='/search' render={() =>
-                    <SearchBooks
-                        currentBooks={this.state.books}
-                        onShelfChange={this.updateShelf}
-                        onBulkShelfChange={this.bulkUpdateShelf}
-                        onBookChecked={this.checkBook}
-                    />
-                }/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className='app'>
+        <Route exact path='/' render={() =>
+          <ListBooks
+            books={this.state.books}
+            onShelfChange={this.updateShelf}
+            onBulkShelfChange={this.bulkUpdateShelf}
+            onBookChecked={this.checkBook}
+          />
+        }/>
+        <Route path='/search' render={() =>
+          <SearchBooks
+            currentBooks={this.state.books}
+            onShelfChange={this.updateShelf}
+            onBulkShelfChange={this.bulkUpdateShelf}
+            onBookChecked={this.checkBook}
+          />
+        }/>
+      </div>
+    )
+  }
 }
 
 export default BooksApp
--- src/BookShelf.js
+++ src/BookShelf.js
@@ -3,36 +3,36 @@ import Book from './Book';
 import PropTypes from 'prop-types';
 
 const BookShelf = (props) => {
-    const booksToDisplay = props.book.map((book) => (
-        <li key={book.id}>
-            <Book
-                id={book.id}
-                title={book.title}
-                authors={book.authors}
-                thumbnail={book.imageLinks && book.imageLinks.thumbnail}
-                shelf={book.shelf}
-                onShelfChange={props.onShelfChange}
-                onBookChecked={props.onBookChecked}
-            />
-        </li>
-    ));
+  const booksToDisplay = props.book.map((book) => (
+    <li key={book.id}>
+      <Book
+        id={book.id}
+        title={book.title}
+        authors={book.authors}
+        thumbnail={book.imageLinks && book.imageLinks.thumbnail}
+        shelf={book.shelf}
+        onShelfChange={props.onShelfChange}
+        onBookChecked={props.onBookChecked}
+      />
+    </li>
+  ));
 
-    return (
-        <div className='bookshelf'>
-            <h2 className='bookshelf-title'>{props.title}</h2>
-            <div className='bookshelf-books'>
-                <ol className='books-grid'>
-                    {booksToDisplay}
-                </ol>
-            </div>
-        </div>
-    )
+  return (
+    <div className='bookshelf'>
+      <h2 className='bookshelf-title'>{props.title}</h2>
+      <div className='bookshelf-books'>
+        <ol className='books-grid'>
+          {booksToDisplay}
+        </ol>
+      </div>
+    </div>
+  )
 };
 
 BookShelf.propTypes = {
-    title: PropTypes.string.isRequired,
-    books: PropTypes.array.isRequired,
-    onShelfChange: PropTypes.func.isRequired
+  title: PropTypes.string.isRequired,
+  books: PropTypes.array.isRequired,
+  onShelfChange: PropTypes.func.isRequired
 };
 
 export default BookShelf;
