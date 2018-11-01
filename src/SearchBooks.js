1d7c6da055967e4691f14cabc25232eda6717185
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-11-01 19:28:21 +0530

    feat: Add SearchBooks class
    Extends Component to create SearchBooks class.

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -4,3 +4,27 @@ import {Link} from 'react-router-dom'
 import Book from './Book';
 import ShelfSelect from './ShelfSelect';
 import PropTypes from 'prop-types'
+
+class SearchBooks extends Component {
+    state = {
+        searchResult: []
+    }
+
+    changeShelf = (updateBook, shelf) => {
+        const bookWithCompleteInfo = this.state.searchResult.find(book => book.id === updateBook.id)
+
+        if (bookWithCompleteInfo) {
+            this.props.onShelfChange(bookWithCompleteInfo, shelf);
+        }
+    }
+
+    bulkShelfChange = (event) => {
+        const targetShelf = event.target.value;
+        this.props.onBulkShelfChange(targetShelf);
+    }
+
+    checkBook = (bookId, checkedStatus) => {
+        const checkedBookWithInfo = this.state.searchResult.find(book => book.id === bookId)
+        this.props.onBookChecked(checkedBookWithInfo, checkedStatus)
+    }
+}
